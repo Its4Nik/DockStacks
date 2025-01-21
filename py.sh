fef40ca98abec90a71f5f937cdf0fcebac4340aa
ItsNik <info@itsnik.de>
2025-01-21 23:19:01 +0100

    Fix: Adjust build'

--- py.sh
+++ py.sh
@@ -4,10 +4,13 @@ python3 -m venv ./
 
 source ./bin/activate
 
+pip install -r ./requirements.txt
+
 ./bin/python3 ./build.py
 
 node ./src/createTemplate.js
 
-pip install -r ./requirements.txt
 
-python3 -m http.server 8080 -d ./src/
+if [[ ! "$CI" = "true" ]]; then
+    python3 -m http.server 8080 -d ./src/
+fi
